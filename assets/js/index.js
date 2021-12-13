4f3c32772916cb7b4d2f584f44d2e135ca312060
Kemuel Nalifuma <kemnygh@gmail.com>
2021-12-13 18:05:53 +0300

    Add style on the main page content

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,20 @@
 $('document').ready(()=>{
   active();
-  animate();
+  anim();
+  // $('.tagline').show(1000).animate({left:"42%"},"1000").animate({left:"100%"})
+
+  // $('.tagline').on('appear',()=>{$(this).show(1000,"slow",()=>{$(this).trigger('anim1')})});
+  $('.tagline').on('anim1',()=>{
+    $('.tagline').animate({left:"42%"},'slow',()=>{
+      $('.tagline').trigger('anim2')
+    });
+  });
+  $('.tagline').on('anim2',()=>{$('.tagline').stop(false,()=>{$('.tagline').trigger('anim3')})});
+  $('.tagline').on('anim3',()=>{$('.tagline').animate({left:"82%"},"slow",()=>{$('.tagline').trigger('anim4')})});
+  $('.tagline').on('anim4',()=>{$('.tagline').hide('slow',()=>{$('.tagline').trigger('anim1')})});
+
+  $('.tagline').trigger('anim1')
+
 });
 
 // active nav item
@@ -22,10 +36,14 @@ function color() {
 
   
 }
-function animate(){
-  img = $('.logo')
+function anim(){
+  var img = $('.logo')
+  var look = 0
+ 
+
   img.animate({height: '300px', opacity: '0.4'}, "slow");
   // img.animate({width: '300px', opacity: '0.8'}, "slow");
-  img.animate({height: '100px', opacity: '0.4'}, "slow");
+  img.animate({height: '100px', opacity: '1'}, "slow");
   // img.animate({width: '100px', opacity: '0.8'}, "slow");
+
 }
