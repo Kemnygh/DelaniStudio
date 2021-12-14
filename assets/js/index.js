a4bb36b66bf220a227c0f88109486b13f99619a7
Kemuel Nalifuma <kemnygh@gmail.com>
2021-12-14 10:23:06 +0300

    Add border on main page, add about us section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,9 @@
 $('document').ready(()=>{
   active();
-  anim();
-  // $('.tagline').show(1000).animate({left:"42%"},"1000").animate({left:"100%"})
+  animateLogo();
+  animateParagraph();
 
-  // $('.tagline').on('appear',()=>{$(this).show(1000,"slow",()=>{$(this).trigger('anim1')})});
-  $('.tagline').on('anim1',()=>{
-    $('.tagline').animate({left:"42%"},'slow',()=>{
-      $('.tagline').trigger('anim2')
-    });
-  });
-  $('.tagline').on('anim2',()=>{$('.tagline').stop(false,()=>{$('.tagline').trigger('anim3')})});
-  $('.tagline').on('anim3',()=>{$('.tagline').animate({left:"82%"},"slow",()=>{$('.tagline').trigger('anim4')})});
-  $('.tagline').on('anim4',()=>{$('.tagline').hide('slow',()=>{$('.tagline').trigger('anim1')})});
-
-  $('.tagline').trigger('anim1')
+  
 
 });
 
@@ -36,14 +26,26 @@ function color() {
 
   
 }
-function anim(){
+function animateLogo(){
   var img = $('.logo')
   var look = 0
  
 
   img.animate({height: '300px', opacity: '0.4'}, "slow");
-  // img.animate({width: '300px', opacity: '0.8'}, "slow");
   img.animate({height: '100px', opacity: '1'}, "slow");
-  // img.animate({width: '100px', opacity: '0.8'}, "slow");
 
+}
+
+function animateParagraph(){
+  $('.tagline').on('disappear',()=>{$('.tagline').fadeOut(4000,()=>{$('.tagline').trigger('appear')})});
+  $('.tagline').on('appear',()=>{
+    $('.tagline').fadeIn(2000,()=>{
+      $('.tagline').trigger('disappear')
+    });
+  });
+  // $('.tagline').on('anim2',()=>{$('.tagline').stop(false,()=>{$('.tagline').trigger('anim3')})});
+  // $('.tagline').on('anim3',()=>{$('.tagline').animate({left:"82%"},"slow",()=>{$('.tagline').trigger('anim4')})});
+  // $('.tagline').on('anim4',()=>{$('.tagline').hide('slow',()=>{$('.tagline').trigger('anim1')})});
+
+  $('.tagline').trigger('appear')
 }
